76a9d521e602dcaa5cf007d5e1a3cb8270335461
Karthik Periagaram <karthik.periagaram@gmail.com>
2014-08-10 12:18:00 -0700

    Parse input arguments
    The script uses argparse to parse input arguments, which are one or more
    ini files. These are summarily passed to the next function which will
    validate each file. Additionally, a helpful message is printed on screen
    if the script is run with -help or with no arguments.

--- archive.py
+++ archive.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-"""Script to create/maintain incremental backups"""
+'''Script to create/maintain incremental backups'''
+
+from argparse import ArgumentParser,RawDescriptionHelpFormatter
 
 def archive_preset( preset, source, destination ):
     pass
@@ -9,5 +11,18 @@ def process_preset_files( preset_files ):
     pass
 
 if __name__ == '__main__':
-    pass
+    parser = ArgumentParser( formatter_class=RawDescriptionHelpFormatter, description = '''
+    Create and maintain incremental backups.
+    Backups are described by presets like this,
+
+        [home]
+        source = /home/user/
+        destination = /mnt/backup/
+
+    A single preset file can contain many such presets.
+    Multiple preset files can be supplied as arguments.''' )
+    parser.add_argument( 'PRESETS', nargs='+', help='One or more INI files containing presets.' )
+    args = parser.parse_args()
+
+    process_preset_files( args.PRESETS )
 
