57e45fc8d2441fabc45197c0dd67f386b67ee4c2
Karthik Periagaram <karthik.periagaram@gmail.com>
2014-08-10 11:52:21 -0700

    Use of preset files to allow backups
    The logic will be as follows. The script will read in configuration
    information from preset files. Preset files are INI files containing the
    name of the preset, the source and the destination. A simple example of
    such a preset file would be:
    
    [home]
    source = /home/user/
    destination = /mnt/backup/
    
    The script will then need to copy over the files in the source into the
    destination under a folder named after today's date (e.g., 2014-08-10/)
    and create a link to it marking it as the latest backup. Subsequent
    backups will be added alongside and each time, the link will point to
    the latest backup.
    
    To do this, first, the input arguments must be parsed. Next, each valid
    preset file is processed and each valid preset is archived.

--- archive.py
+++ archive.py
@@ -2,3 +2,12 @@
 
 """Script to create/maintain incremental backups"""
 
+def archive_preset( preset, source, destination ):
+    pass
+
+def process_preset_files( preset_files ):
+    pass
+
+if __name__ == '__main__':
+    pass
+
